3d9a00dd75aa30dd44ae0e8b4ec84039154cc179
Rafi Chowdhury <rafi1220@gmail.com>
2026-04-29 07:14:05 -0500

    Added core file

---
+++ src/iam-threat-detection/__init__.py
@@ -0,0 +1,10 @@
+"""
+IAM Threat Detection System - Enterprise-grade identity threat detection
+"""
+
+__version__ = "1.0.0"
+__author__ = "Solution Architect Team"
+
+from .core.config import settings
+
+__all__ = ["settings"]
